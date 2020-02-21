33c9a00456fa72422938f7d8d1e9cca68e842d72
Robin <RobinFlohr@gmail.com>
2020-02-22 00:01:26 +0100

    we can now add the git revision to Sentry...

--- src/main/java/im/redpanda/App.java
+++ src/main/java/im/redpanda/App.java
@@ -4,7 +4,10 @@ import im.redpanda.core.Server;
 import io.sentry.Sentry;
 import io.sentry.event.UserBuilder;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Enumeration;
 import java.util.jar.Attributes;
@@ -17,24 +20,29 @@ public class App {
     public static void main(String[] args) throws IOException {
         System.out.print("Starting redpanda " + new App().getClass().getPackage().getImplementationVersion());
 
-        Enumeration<URL> resources = new App().getClass().getClassLoader()
-                .getResources("META-INF/MANIFEST.MF");
-        while (resources.hasMoreElements()) {
-            try {
-                Manifest manifest = new Manifest(resources.nextElement().openStream());
-                // check that this is your manifest and do what you need or get the next one
-
-//                System.out.println(" build number: " + manifest.getMainAttributes().getValue("Implementation-Build"));
-
-            } catch (IOException E) {
-                // handle
-            }
-        }
+//        Enumeration<URL> resources = new App().getClass().getClassLoader()
+//                .getResources("META-INF/MANIFEST.MF");
+//        while (resources.hasMoreElements()) {
+//            try {
+//                Manifest manifest = new Manifest(resources.nextElement().openStream());
+//                // check that this is your manifest and do what you need or get the next one
+//
+////                System.out.println(" build number: " + manifest.getMainAttributes().getValue("Implementation-Build"));
+//
+//            } catch (IOException E) {
+//                // handle
+//            }
+//        }
 
 
         Sentry.init("https://eefa8afdcdb7418995f6306c136546c7@sentry.io/1400313");
 
-        Sentry.getContext().addTag("release", new App().getClass().getPackage().getImplementationVersion());
+        String gitRev = readGitProperties();
+        if (gitRev != null) {
+            Sentry.getContext().addTag("release", gitRev);
+            System.out.println("found git revision: " + gitRev);
+        }
+
 
         Server.start();
 
@@ -43,6 +51,24 @@ public class App {
 
     public String getHelloWorld() {
         return "Hello World!";
+    }
+
+    public static String readGitProperties() throws IOException {
+        String gitRev = null;
+        InputStream is = new App().getClass().getResourceAsStream("/git.properties");
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        String line;
+        while ((line = br.readLine()) != null) {
+
+            if (line.startsWith("git.commit.id.abbrev=")) {
+                gitRev = line.split("=")[1];
+            }
+        }
+        br.close();
+        isr.close();
+        is.close();
+        return gitRev;
     }
 
 
