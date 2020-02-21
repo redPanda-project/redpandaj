2d23baa8a9bd851ca901bd0313be93beab69f072
Robin <RobinFlohr@gmail.com>
2020-02-21 23:24:26 +0100

    readded the noncritical parts

--- src/test/java/im/redpanda/core/PeerInHandshakeTest.java
+++ src/test/java/im/redpanda/core/PeerInHandshakeTest.java
@@ -3,61 +3,71 @@ package im.redpanda.core;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Set;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class PeerInHandshakeTest {
 
     @Test
     public void addConnection() throws IOException, InterruptedException {
 
-//        Log.LEVEL = 10000;
-//
-//        ConnectionHandler connectionHandler = new ConnectionHandler();
-//        connectionHandler.start();
-//
-//
-//        //lets block the main selector worker
-//        connectionHandler.selectorLock.lock();
-//        connectionHandler.selector.wakeup();
-//
-//
-//        SocketChannel open = SocketChannel.open();
-//        open.configureBlocking(false);
-//
-//        Thread.sleep(100);
-//
-//        boolean alreadyConnected = open.connect(new InetSocketAddress("127.0.0.1", Server.MY_PORT));
-//
-//        PeerInHandshake peerInHandshake = new PeerInHandshake("127.0.0.1", open);
-//
-//
-//        //lets not read the data by the main thread by using the alreadyConnected value false....
-//        peerInHandshake.addConnection(false);
-//
-//        int cnt = 0;
-//        while (cnt < 100) {
-//            cnt++;
-//            int select = connectionHandler.selector.select();
-////            System.out.println("select: " + select);
-//            if (select != 0) {
-//                break;
-//            }
-//        }
-//
-//        Set<SelectionKey> selectionKeys = connectionHandler.selector.selectedKeys();
-//
-//        assertFalse(selectionKeys.isEmpty());
-//
-//        assertTrue(selectionKeys.size() == 2);
-//
-//        for (SelectionKey key : selectionKeys) {
-//            if (key.channel() instanceof ServerSocketChannel) {
-//                continue;
-//            }
-//            assertTrue(key.isConnectable());
-//        }
-//
-//
-//        open.finishConnect();
+        Log.LEVEL = 10000;
+
+        ConnectionHandler connectionHandler = new ConnectionHandler();
+        connectionHandler.start();
+
+
+        //lets block the main selector worker
+        connectionHandler.selectorLock.lock();
+        connectionHandler.selector.wakeup();
+
+
+        SocketChannel open = SocketChannel.open();
+        open.configureBlocking(false);
+
+        Thread.sleep(100);
+
+        boolean alreadyConnected = open.connect(new InetSocketAddress("127.0.0.1", Server.MY_PORT));
+
+        PeerInHandshake peerInHandshake = new PeerInHandshake("127.0.0.1", open);
+
+
+        //lets not read the data by the main thread by using the alreadyConnected value false....
+        peerInHandshake.addConnection(false);
+
+        int cnt = 0;
+        while (cnt < 100) {
+            cnt++;
+            int select = connectionHandler.selector.select();
+//            System.out.println("select: " + select);
+            if (select != 0) {
+                break;
+            }
+        }
+
+        Set<SelectionKey> selectionKeys = connectionHandler.selector.selectedKeys();
+
+
+
+        assertFalse(selectionKeys.isEmpty());
+
+        assertTrue(selectionKeys.size() == 2);
+
+        for (SelectionKey key : selectionKeys) {
+            if (key.channel() instanceof ServerSocketChannel) {
+                continue;
+            }
+            assertTrue(key.isConnectable());
+        }
+
+
+        open.finishConnect();
 
 
 //        peerInHandshake.getKey().interestOps(0);
