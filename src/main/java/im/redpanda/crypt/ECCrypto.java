8d7fe520c999744bcfe5e355311b905253b7cfdd
Dr. Robin Braun <pY4x3g@users.noreply.github.com>
2026-01-04 21:08:57 +0100

    fix: resolve raw type warnings and unchecked casts

--- src/main/java/im/redpanda/crypt/ECCrypto.java
+++ src/main/java/im/redpanda/crypt/ECCrypto.java
@@ -28,7 +28,7 @@ public class ECCrypto {
       Provider p[] = Security.getProviders();
       for (int i = 0; i < p.length; i++) {
         System.out.println(p[i]);
-        for (Enumeration e = p[i].keys(); e.hasMoreElements(); ) {
+        for (Enumeration<Object> e = p[i].keys(); e.hasMoreElements(); ) {
           System.out.println("\t" + e.nextElement());
         }
       }
